bc1a4f048e93ac20801404d23bef59f624102d09
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2025-01-11 12:14:56 -0600

    undoing

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,30 +1,8 @@
 // filepath: /c:/Users/James/Leaf_And_Luggage/leaf_and_luggage/api/routes/index.js
 const express = require('express');
 const router = express.Router();
-const client = require('../redisConfig');
-
-// Test Redis connection
-router.get('/test-redis', (req, res) => {
-  const testKey = 'test';
-  const testValue = 'Redis is connected!';
-
-  client.set(testKey, testValue, (err) => {
-    if (err) {
-      return res.status(500).send('Error setting value in Redis');
-    }
-
-    client.get(testKey, (err, value) => {
-      if (err) {
-        return res.status(500).send('Error getting value from Redis');
-      }
-
-      res.send(`Value from Redis: ${value}`);
-    });
-  });
-});
-
-// Example endpoint with caching middleware
 const cacheMiddleware = require('../cacheMiddleware');
+
 router.get('/some-endpoint', cacheMiddleware, (req, res) => {
   // Your endpoint logic
   res.json({ data: 'This is some data' });
